f6833b5fd58b35f834ce8ce98807f7391e89f2d1
Gabriela Papp <gpgabika@gmail.com>
2017-07-26 21:10:39 +0100

    js code added

---
+++ app.js
@@ -0,0 +1,21 @@
+  window.onload = function () {
+                document.querySelector('#submit').onclick = function () {
+                var input = document.querySelector('#pick_topic').value;
+
+                var api = $.get('http://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=API_KEY&limit=20')
+                        .done(function (result) {
+                            
+                                for(var i=0;i< result.data.length;i++){
+                                var div = document.querySelector('.col');
+                                var img = document.createElement('img');
+                                
+                                img.src = result.data[i].images.original.url;
+                                img.className = 'thumb';
+                                img.style.width = '200px';
+                                div.appendChild(img);
+                            }
+                            });
+
+                        };
+
+            };
