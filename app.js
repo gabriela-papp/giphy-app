03fc5530a39bccb5473268062dac5e0e67d09d6e
Gabriela Papp <gpgabika@gmail.com>
2017-07-28 23:38:53 +0100

    padding added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
                 document.querySelector('#submit').onclick = function () {
                 var input = document.querySelector('#pick_topic').value;
 
-                var api = $.get('http://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=API_KEY&limit=20')
+                var api = $.get('http://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=API-KEY&limit=35')
                         .done(function (result) {
                             
                                 for(var i=0;i< result.data.length;i++){
@@ -10,7 +10,7 @@
                                 var img = document.createElement('img');
                                 
                                 img.src = result.data[i].images.original.url;
-                                img.className = 'thumb col col-md-4 col-xs-';
+                                img.className = 'thumb col col-md-4 col-xs-12';
                                 
                                 div.appendChild(img);
                             }
